5c2654e3f6d28d51edf6e5c726ba8d1495ac7208
Ronnie Rodrigues Pereira <ronnie.rodriguespereira@genomicsengland.co.uk>
2024-06-11 16:22:34 +0100

    Changed wf call to avoid setting working directory

--- submit.sh
+++ submit.sh
@@ -15,15 +15,15 @@ module purge
 module load singularity/4.1.1 nextflow/23.10
 
 # VARIABLES TO EDIT PRIOR TO SUBMISSION
-driver_discovery='/path/to/dir'
-scratchdir='/re_scratch/path/to/dir'
+DRIVER_DISCOVERY='/path/to/dir'
+SCRATCHDIR='/re_scratch/path/to/dir'
 
 # RUN WORKFLOW
 cd $driver_discovery
-nextflow run ./main.nf \
+nextflow run "${DRIVER_DISCOVERY}"./main.nf \
     --variant_type "coding" \
     --sample_file "/pgen_int_work/BRS/cancer_dev/discovery/somatic_driver_discovery/input/min_luad_vcf.txt" \
     --region_file "/pgen_int_work/BRS/cancer_dev/discovery/somatic_driver_discovery/resources/global/coding_CDS.tsv.gz" \
-    --scratchdir "/nas/weka.gel.zone/re_scratch/rrodriguespereira/ssd" \
-    -c nextflow.config \
+    --scratchdir "${SCRATCHDIR}" \
+    -c "${DRIVER_DISCOVERY}"/nextflow.config \
     -profile cluster
